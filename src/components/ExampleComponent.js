2fa716dea50912e230a0bd6e44d7f0b984c86635
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-04-22 22:35:16 +0300

    final

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -4,8 +4,7 @@ import React from "react";
 function ExampleComponent() {
   return (
     <div className="example">
-      <img
-        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+      <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
